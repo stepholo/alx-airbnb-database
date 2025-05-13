af6dba34983b56b589f49c6f609130ba81bcbdb3
stepholo <oloobrian89@gmail.com>
2025-05-13 16:01:17 +0300

    Joining Statement

--- database-adv-script/joins_queries.sql
+++ database-adv-script/joins_queries.sql
@@ -4,7 +4,7 @@ SELECT * FROM "user" INNER JOIN booking ON "user".user_id = booking.user_id;
 
 -- Write a query using a LEFT JOIN to retrieve all properties and their reviews, including properties that have no reviews.
 
-SELECT * FROM review LEFT JOIN property ON property.property_id = review.property_id;
+SELECT * FROM review LEFT JOIN property ON property.property_id = review.property_id ORDER BY rating ASC;
 
 -- Write a query using a FULL OUTER JOIN to retrieve all users and all bookings, even if the user has no booking or a booking is not linked to a user.
 
