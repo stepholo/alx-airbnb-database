080e77788bcddc53a852c6e00b49eccaecc5703a
stepholo <oloobrian89@gmail.com>
2025-05-13 22:53:15 +0300

    Implementing Indexing

--- database-adv-script/database_index.sql
+++ database-adv-script/database_index.sql
@@ -1,6 +1,10 @@
 -- Identify high-usage columns in your User, Booking, and Property tables
 
+EXPLAIN SELECT * FROM "user" INNER JOIN property ON "user".user_id = property.user_id;
+
 CREATE INDEX idx_property_host_id ON property (host_id);
 CREATE INDEX idx_booking_property_id ON booking (property_id);
 CREATE INDEX idx_booking_user_id ON booking (user_id);
 CREATE INDEX idx_user_email ON "user" (email);
+
+EXPLAIN ANALYZE SELECT * FROM "user" INNER JOIN property ON "user".user_id = property.user_id;
