5b9baf370f3bb360265de4011c01226fe527e432
stepholo <oloobrian89@gmail.com>
2025-05-13 15:58:41 +0300

    Joining Statement

---
+++ database-adv-script/README.md
@@ -0,0 +1,1 @@
+# Write Complex Queries with Joins
---
+++ database-adv-script/joins_queries.sql
@@ -0,0 +1,11 @@
+-- Write a query using an INNER JOIN to retrieve all bookings and the respective users who made those bookings.
+
+SELECT * FROM "user" INNER JOIN booking ON "user".user_id = booking.user_id;
+
+-- Write a query using a LEFT JOIN to retrieve all properties and their reviews, including properties that have no reviews.
+
+SELECT * FROM review LEFT JOIN property ON property.property_id = review.property_id;
+
+-- Write a query using a FULL OUTER JOIN to retrieve all users and all bookings, even if the user has no booking or a booking is not linked to a user.
+
+SELECT * FROM "user" FULL OUTER JOIN booking ON "user".user_id = booking.user_id;
